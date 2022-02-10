65d82ea6004deaeea40023354ee44f91fc323a9e
iCaitlyn <55845572+iCaitlyn@users.noreply.github.com>
2022-02-10 22:16:42 +0800

    Create DeathLocation.java

---
+++ src/private/mc/deathlocation/DeathLocation.java
@@ -0,0 +1,27 @@
+package private.mc.deathlocation;
+
+import org.bukkit.event.EventHandler;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class DeathLocation extends JavaPlugin implements Listener
+{
+
+}
